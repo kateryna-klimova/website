b8c8179770de2314c0c4d4588bb41a9be680d523
Kateryna Klimova <katrynaklimova@gmail.com>
2019-02-27 23:00:41 +0200

    adding files

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/IMG_4056.JPG') {
+      myImage.setAttribute ('src','images/IMG_4053.JPG');
+    } else {
+      myImage.setAttribute ('src','images/IMG_4056.JPG');
+    }
+}
+
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+//
+// function setUserName() {
+//   var myName = prompt('Please enter your name.');
+//   localStorage.setItem('name', myName);
+//   myHeading.textContent = 'Mozilla is cool, ' + myName;
+// }
+//
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   var storedName = localStorage.getItem('name');
+//   myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
+//
+// myButton.onclick = function() {
+//   setUserName();
+// }
